e8d976d7717b0ad7128fea2355fe38a6b79e2197
raghav393 <raghav393@gmail.com>
2025-08-18 02:39:38 +0530

    Add files via upload

--- src/silver/data_cleansing.py
+++ src/silver/data_cleansing.py
@@ -15,23 +15,35 @@ from shared.utils import *
 
 #Function read data from bronze delta tables
 def read_delta_tables():
-    df_customers=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/customers").select("Customer_Name","Customer_ID","Country")
-    df_products=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/products").select("Product_ID","Category","Sub_Category")
-    df_orders=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/order").select("Order_ID","Order_Date","Customer_ID","Product_ID","Profit")
-    return {"customers":df_customers,"products":df_products,"orders":df_orders}
+    try:
+        df_customers=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/customers").select("Customer_Name","Customer_ID","Country")
+        df_products=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/products").select("Product_ID","Category","Sub_Category")
+        df_orders=spark.read.format("delta").load("abfss://bronze@dbinterviewvivek1.dfs.core.windows.net/order").select("Order_ID","Order_Date","Customer_ID","Product_ID","Profit")
+        return {"customers":df_customers,"products":df_products,"orders":df_orders}
+    except Exception as e:
+        raise FileNotFoundError("File not found and failed with error {e}")
 
 #Pandas UDF to parse different date formats
 @pandas_udf(DateType())
 def format_dates(date_series):
-    return date_series.apply(lambda x: parser.parse(x) if x else None)
+    try:
+        return date_series.apply(lambda x: parser.parse(x) if x else None)
+    except Exception as e:
+        raise Exception(f"Error parsing date: {e}")
 
 #Function to format date
 def date_formatting(df):
-    df_orderscleansed=df.withColumn("Order_Date",format_dates(col("Order_Date")))
-    return df_orderscleansed
+    try:
+        df_orderscleansed=df.withColumn("Order_Date",format_dates(col("Order_Date")))
+        return df_orderscleansed
+    except Exception as e:
+        raise Exception(f"Error formatting date: {e}")
 
 #Function to load cleansing rules config file
 def load_config_rules():
-    config_path = "abfss://config@dbinterviewvivek1.dfs.core.windows.net/rules_config.json"
-    config = load_config_from_adls(config_path,spark)
-    return config
+    try:
+        config_path = "abfss://config@dbinterviewvivek1.dfs.core.windows.net/rules_config.json"
+        config = load_config_from_adls(config_path,spark)
+        return config
+    except Exception as e:
+        raise Exception(f"Error loading config file: {e}")
